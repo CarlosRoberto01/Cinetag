4a0cb3bd7029a7ea8dcbf7252b838943f973d2cd
Carlos Roberto <carloos.bezerra@hotmail.com>
2023-08-09 17:24:02 -0300

    Criando cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from 'components/Banner';
 import Cabecalho from 'components/Cabecalho';
+import Card from 'components/Card';
 import Rodape from 'components/Rodape';
 import Titulo from 'components/Titulo';
 import React from 'react';
@@ -12,6 +13,7 @@ export default function Inicio() {
       <Titulo>
         <h1>Lugar para guardar os filmes</h1>
       </Titulo>
+      <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png'></Card>
       <Rodape />
     </>
   )

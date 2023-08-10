5949f2b079911765cb787398ee7335759575fe69
Carlos Roberto <carloos.bezerra@hotmail.com>
2023-08-10 16:02:50 -0300

    Aplicando função favoritar

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styles from './Card.module.css';
-import iconeFavoritar from './favoritar.png'
+import iconeFavoritar from './favoritar.png';
+import { useFavoritoContext } from 'components/Contexto/Favoritos';
+import iconeDesfavoritar from './desfavoritar.png';
 
 export default function Card({ id, titulo, capa }) {
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa}></img>
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar}></img>
+            <img
+                src={icone}
+                alt="Favoritar filme"
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }}
+            >
+            </img>
         </div>
     )
 }
--- src/components/Contexto/Favoritos.js
+++ src/components/Contexto/Favoritos.js
@@ -25,7 +25,7 @@ export function useFavoritoContext(){
             return setFavorito(novaLista);
         }
 
-        novaLista.slice(novaLista.indexOf(novoFavorito), 1);
+        novaLista = favorito.filter((fav) => fav.id !== novoFavorito.id);
         return setFavorito(novaLista);
     }
     return {

9eba1fd0024682fdf962b0641c9bdb02dcd2d694
Carlos Roberto <carloos.bezerra@hotmail.com>
2023-08-16 18:56:21 -0300

    Rota dinâmica para o card e player

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import FavoritosProvider from "components/Contexto/Favoritos";
 import Rodape from "components/Rodape";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -15,6 +16,7 @@ export default function AppRoutes() {
                     <Routes>
                         <Route path="/" element={<Inicio />}></Route>
                         <Route path="/Favoritos" element={<Favoritos />}> </Route>
+                        <Route path="/:id" element={<Player />}> </Route>
                     </Routes>
                 </FavoritosProvider>
             </Container>

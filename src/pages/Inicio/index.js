83096df0b75f5ad97e8869cc7da55c0b1beb4656
Carlos Roberto <carloos.bezerra@hotmail.com>
2023-08-17 09:53:21 -0300

    Externalizando a API

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,10 +2,20 @@ import Banner from 'components/Banner';
 import styles from './Inicio.module.css'
 import Card from 'components/Card';
 import Titulo from 'components/Titulo';
-import React from 'react';
-import videos from 'json/db.json'
+import React, { useEffect, useState } from 'react';
 
 export default function Inicio() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/CarlosRoberto01/Cinetag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+          setVideos(dados)
+      })
+  }, [])
+
   return (
     <>
       <Banner imagem="Home" />
